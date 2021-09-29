2bf749d4cd7aa1605c76d74e12d33ebe35653e43
KEKE046 <KEKE_046@pku.edu.cn>
2021-09-29 13:16:29 +0800

    gramar

--- kern/monitor.c
+++ kern/monitor.c
@@ -29,7 +29,8 @@ static struct Command commands[] = {
 
 /***** Implementations of basic kernel monitor commands *****/
 
-int mon_color_test(int argc, char ** argv, struct Trapframe * tf) {
+int
+mon_color_test(int argc, char ** argv, struct Trapframe * tf) {
 	static const int forecolor[] = {
 		30, 31, 32, 33, 34, 35, 36, 37,
 		90, 91, 92, 93, 94, 95, 96, 97
