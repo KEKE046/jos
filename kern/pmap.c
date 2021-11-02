e644c97d48153eab6353ae5399f7dd867b09c29a
KEKE046 <KEKE_046@pku.edu.cn>
2021-11-02 11:43:25 +0800

    exercise 3

--- kern/pmap.c
+++ kern/pmap.c
@@ -277,6 +277,11 @@ mem_init_mp(void)
 	//
 	// LAB 4: Your code here:
 
+	for(size_t i = 0; i < NCPU; i++) {
+		uintptr_t kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
+		boot_map_region(kern_pgdir, kstacktop_i - KSTKSIZE, KSTKSIZE, PADDR(percpu_kstacks[i]), PTE_P|PTE_W);
+		// boot_map_region(kern_pgdir, kstacktop_i - KSTKSIZE - KSTKGAP, kstacktop_i - KSTKSIZE, 0, PTE_P);
+	}
 }
 
 // --------------------------------------------------------------
