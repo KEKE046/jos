46a8aa312c1ade6f2be30cc4e1699b82f6a71104
KEKE046 <KEKE_046@pku.edu.cn>
2021-11-02 15:31:22 +0800

    exercise 11

--- lib/pgfault.c
+++ lib/pgfault.c
@@ -29,7 +29,10 @@ set_pgfault_handler(void (*handler)(struct UTrapframe *utf))
 	if (_pgfault_handler == 0) {
 		// First time through!
 		// LAB 4: Your code here.
-		sys_env_set_pgfault_upcall(thisenv, _pgfault_upcall);
+		r = sys_env_set_pgfault_upcall(0, _pgfault_upcall);
+		if(r < 0) {
+			panic("set_pgfault_handler fault %e", r);
+		}
 		// panic("set_pgfault_handler not implemented");
 	}
 
