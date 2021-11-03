aa2f1173971b62072686aef31def1fe71bc66d0d
KEKE046 <KEKE_046@pku.edu.cn>
2021-11-03 17:05:44 +0800

    very beautiful

--- fs/serv.c
+++ fs/serv.c
@@ -342,11 +342,11 @@ umain(int argc, char **argv)
 {
 	static_assert(sizeof(struct File) == 256);
 	binaryname = "fs";
-	cprintf("FS is running\n");
+	logp("FS is running");
 
 	// Check that we are able to do I/O
 	outw(0x8A00, 0x8A00);
-	cprintf("FS can do I/O\n");
+	logi("FS can do I/O");
 
 	serve_init();
 	fs_init();
--- user/sh.c
+++ user/sh.c
@@ -35,7 +35,7 @@ again:
 
 		case 'w':	// Add an argument
 			if (argc == MAXARGS) {
-				cprintf("too many arguments\n");
+				loge("too many arguments");
 				exit();
 			}
 			argv[argc++] = t;
@@ -44,7 +44,7 @@ again:
 		case '<':	// Input redirection
 			// Grab the filename from the argument list
 			if (gettoken(0, &t) != 'w') {
-				cprintf("syntax error: < not followed by word\n");
+				loge("syntax error: < not followed by word");
 				exit();
 			}
 			// Open 't' for reading as file descriptor 0
@@ -56,17 +56,25 @@ again:
 			// then close the original 'fd'.
 
 			// LAB 5: Your code here.
-			panic("< redirection not implemented");
+			// panic("< redirection not implemented");
+			if ((fd = open(t, O_RDONLY)) < 0) {
+				loge("open %s for write: %e", t, fd);
+				exit();
+			}
+			if (fd != 1) {
+				dup(fd, 1);
+				close(fd);
+			}
 			break;
 
 		case '>':	// Output redirection
 			// Grab the filename from the argument list
 			if (gettoken(0, &t) != 'w') {
-				cprintf("syntax error: > not followed by word\n");
+				loge("syntax error: > not followed by word");
 				exit();
 			}
 			if ((fd = open(t, O_WRONLY|O_CREAT|O_TRUNC)) < 0) {
-				cprintf("open %s for write: %e", t, fd);
+				loge("open %s for write: %e", t, fd);
 				exit();
 			}
 			if (fd != 1) {
@@ -77,13 +85,13 @@ again:
 
 		case '|':	// Pipe
 			if ((r = pipe(p)) < 0) {
-				cprintf("pipe: %e", r);
+				loge("pipe: %e", r);
 				exit();
 			}
 			if (debug)
-				cprintf("PIPE: %d %d\n", p[0], p[1]);
+				logd("PIPE: %d %d", p[0], p[1]);
 			if ((r = fork()) < 0) {
-				cprintf("fork: %e", r);
+				loge("fork: %e", r);
 				exit();
 			}
 			if (r == 0) {
@@ -145,7 +153,7 @@ runit:
 
 	// Spawn the command!
 	if ((r = spawn(argv[0], (const char**) argv)) < 0)
-		cprintf("spawn %s: %e\n", argv[0], r);
+		loge("spawn %s: %e", argv[0], r);
 
 	// In the parent, close all file descriptors and wait for the
 	// spawned command to exit.
@@ -194,12 +202,12 @@ _gettoken(char *s, char **p1, char **p2)
 
 	if (s == 0) {
 		if (debug > 1)
-			cprintf("GETTOKEN NULL\n");
+			logd("GETTOKEN NULL");
 		return 0;
 	}
 
 	if (debug > 1)
-		cprintf("GETTOKEN: %s\n", s);
+		logd("GETTOKEN: %s", s);
 
 	*p1 = 0;
 	*p2 = 0;
@@ -208,7 +216,7 @@ _gettoken(char *s, char **p1, char **p2)
 		*s++ = 0;
 	if (*s == 0) {
 		if (debug > 1)
-			cprintf("EOL\n");
+			logd("EOL");
 		return 0;
 	}
 	if (strchr(SYMBOLS, *s)) {
@@ -217,7 +225,7 @@ _gettoken(char *s, char **p1, char **p2)
 		*s++ = 0;
 		*p2 = s;
 		if (debug > 1)
-			cprintf("TOK %c\n", t);
+			logd("TOK %c", t);
 		return t;
 	}
 	*p1 = s;
@@ -227,7 +235,7 @@ _gettoken(char *s, char **p1, char **p2)
 	if (debug > 1) {
 		t = **p2;
 		**p2 = 0;
-		cprintf("WORD: %s\n", *p1);
+		logd("WORD: %s", *p1);
 		**p2 = t;
 	}
 	return 'w';
@@ -253,7 +261,7 @@ gettoken(char *s, char **p1)
 void
 usage(void)
 {
-	cprintf("usage: sh [-dix] [command-file]\n");
+	loge("usage: sh [-dix] [command-file]");
 	exit();
 }
 
@@ -298,21 +306,21 @@ umain(int argc, char **argv)
 		buf = readline(interactive ? AT_BRI_GRN "$ " AT_RESET : NULL);
 		if (buf == NULL) {
 			if (debug)
-				cprintf("EXITING\n");
+				logd("EXITING");
 			exit();	// end of file
 		}
 		if (debug)
-			cprintf("LINE: %s\n", buf);
+			logd("LINE: %s", buf);
 		if (buf[0] == '#')
 			continue;
 		if (echocmds)
-			printf("# %s\n", buf);
+			printf("# %s", buf);
 		if (debug)
-			cprintf("BEFORE FORK\n");
+			logd("BEFORE FORK\n");
 		if ((r = fork()) < 0)
 			panic("fork: %e", r);
 		if (debug)
-			cprintf("FORK: %d\n", r);
+			logd("FORK: %d", r);
 		if (r == 0) {
 			runcmd(buf);
 			exit();
